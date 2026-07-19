b05d97c3bdb1c28e5cdf5c8cbdd61808e1d7e600
Ahlem863 <ahlem.taam@ept.ucar.tn>
2026-07-19 19:44:08 +0100

    Connexion au backend en ligne

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://gestion-conges-rc-backend.onrender.com/api',
 });
 
 // Intercepteur : ajoute automatiquement le token JWT à chaque requête si l'utilisateur est connecté
